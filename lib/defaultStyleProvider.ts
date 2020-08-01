830fb170837e9b5c75b9be8fbbac7078e758cf2d
Paul Gerarts <paulgerarts@weave.nl>
2020-08-01 21:37:22 +0200

    Move default styles

--- lib/defaultStyleProvider.ts
+++ lib/defaultStyleProvider.ts
@@ -1,42 +1,5 @@
+import { defaultStyles, StyleObject } from './defaultStyles';
 import { StyleProvider } from './StyleProvider';
-
-export interface Color {
-    color: string;
-    contrast: string;
-}
-
-export interface Palette {
-    accent: Color;
-    primary: Color;
-    secondary: Color;
-}
-
-export interface StyleObject {
-    fontFamily: string;
-    fontSize: string;
-    lineHeight: string;
-    palette: Palette;
-}
-
-export const defaultStyles: StyleObject = {
-    fontFamily: 'sans-serif',
-    fontSize: '14px',
-    lineHeight: '1.2',
-    palette: {
-        accent: {
-            color: '#2979FF',
-            contrast: 'white',
-        },
-        primary: {
-            color: '#2196F3',
-            contrast: 'black',
-        },
-        secondary: {
-            color: '#607D8B',
-            contrast: 'white',
-        },
-    },
-};
 
 // StyleProvider object
 const styleProviderObject: StyleProvider<StyleObject> = new StyleProvider(defaultStyles);
